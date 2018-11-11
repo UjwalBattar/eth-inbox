844640d4b0c0f841129d7a32734d8196f858217f
Ujwal Battar <ujwal.battar@gmail.com>
2018-11-10 18:19:30 -0800

    deploy contract on test network

---
+++ playground/car.test.js
@@ -0,0 +1,25 @@
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+
+//     drive() {
+//         return 'vroooooom';
+//     }
+// }
+
+// var car;
+
+// beforeEach(() => {
+//     car = new Car();
+// });
+
+// describe('Car', () => {
+//     it('should be able to park', () => {
+//         assert.equal(car.park(), 'stopped');
+//     });
+
+//     it('should be able to drive', () => {
+//         assert.equal(car.drive(), 'vroooooom');
+//     });
+// });
