4bd033e058f32bb080d798d79dd933cf6e55439a
Ujwal Battar <ujwal.battar@gmail.com>
2018-11-10 19:41:06 -0800

    deploy Inbox contract to Rinkeby and interact via Etherscan and Remix in browser

---
+++ deploy.js
@@ -0,0 +1,39 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {
+    interface,
+    bytecode
+} = require('./compile');
+
+const {
+    myMnemonic,
+    infuraLink
+} = require('./config');
+
+const provider = new HDWalletProvider(
+    myMnemonic,
+    infuraLink
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('attempting to deploy from ', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: ['Hi there!']
+        })
+        .send({
+            gas: '1000000',
+            from: accounts[0]
+        });
+
+    console.log('Constract deployed to ', result.options.address);
+}
+
+// rinkeby,etherscan.io
+
+deploy();
